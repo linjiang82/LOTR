7a1b1e8611a2a1b2ace1b49b342af15030618bb0
jiang.lin <jiang.lin@ladbrokes.com.au>
2023-10-20 12:44:49 +1000

    Initial Commit

---
+++ LOTR/LOTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRApp.swift
+//  LOTR
+//
+//  Created by Jiang Lin on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LOTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
